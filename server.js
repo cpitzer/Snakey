44c19d8558c30c8cf479a2f94379db87c78198ba
Caroline <cpitzer11@gmail.com>
2019-10-03 09:59:21 -0500

    first

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const express = require('express'),
-    app = express(), 
-    path = require('path');
-    bodyParser = require('body-parser');
-​
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-​
+const express = require('express');
+const app = express()
+const path = require('path');
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.use('/', express.static(path.join(__dirname + "/client")));
-​
-​
-app.listen(3000);
+
+app.listen(PORT);
